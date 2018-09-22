db3e05b208b10ecc7de65771663556536e46897c
Jacob Phillips <jifalops@gmail.com>
2018-09-22 14:16:40 -0400

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [0.0.1] - September 22, 2018
+
+* First release.
---
+++ example/main.dart
@@ -0,0 +1,13 @@
+import 'package:locales/locales.dart';
+import 'package:locales/currency_codes.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  final locale = Locales.english_united_states;
+  final currencyCode = CurrencyCodes.united_states_dollar;
+  final format = NumberFormat.simpleCurrency(
+      locale: '$locale', name: '$currencyCode', decimalDigits: 2);
+  print(locale); // en_US
+  print(currencyCode); // USD
+  print(format.format(123.456)); // $123.46
+}
